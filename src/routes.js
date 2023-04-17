11c6aa1c641aa760349e09c70d6469c5db07481f
Brownoak <helanii93@gmail.com>
2023-04-17 11:50:22 +0300

    updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 import React from 'react'
 const Progress = React.lazy(()=>import('./views/Progress.js')) 
 const Order = React.lazy(() => import('./views/Order.js'))
+const Drivers = React.lazy(()=> import('./views/Drivers.js'))
+const Customer = React.lazy(()=> import('./views/Customer.js'))
+const Employees = React.lazy(()=> import('./views/Employees.js'))
+const ServiceProviders = React.lazy(()=> import('./views/ServiceProviders.js'))
 
 const routes = [
     { path: '/', exact: true, name: 'Home' },
     { path: '/order', name: 'Order', element: Order },
     { path: '/progress', name: 'Progress', element: Progress },
+    { path: '/drivers', name: 'Drivers', element: Drivers },
+    { path: '/customer', name: 'Customer', element: Customer },
+    { path: '/employees', name: 'Employees', element: Employees },
+    { path: '/providers', name: 'ServiceProviders', element: ServiceProviders },
+
 
 ]
 
