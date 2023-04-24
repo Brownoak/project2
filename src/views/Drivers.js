3bc3c217f298a164afb7d65929fa1bc5b36c665e
Brownoak <helanii93@gmail.com>
2023-04-24 09:42:22 +0300

    added drivers

---
+++ src/views/Drivers.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+import {
+  CAvatar,
+  CCard,
+  CCardBody,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CHeaderNav,
+} from "@coreui/react";
+import { TableDropdown } from "../components/index";
+
+import CIcon from "@coreui/icons-react";
+import {
+  cilPeople,
+  
+} from "@coreui/icons";
+
+import avatar1 from "../assets/images/avatars/1.jpg";
+import avatar2 from "../assets/images/avatars/2.jpg";
+import avatar3 from "../assets/images/avatars/3.jpg";
+import avatar4 from "../assets/images/avatars/4.jpg";
+import avatar5 from "../assets/images/avatars/5.jpg";
+import avatar6 from "../assets/images/avatars/6.jpg";
+
+const Drivers = () => {
+  const tableExample = [
+    {
+      avatar: { src: avatar1, status: "success" },
+      name: "lidya desalegn",
+      phone: "0911111111",
+      account: 1000234578400,
+      status: "deliverd",
+    },
+    {
+      avatar: { src: avatar5, status: "danger" },
+
+      name: "seble stayew",
+
+      phone: "0922222222",
+      account: 1000234578400,
+      status: "pending",
+    },
+    {
+      avatar: { src: avatar2, status: "warning" },
+      name: "yohna desalegn",
+      phone: "093333333",
+      account: 1000234578400,
+      status: "calncelled",
+    },
+    {
+      avatar: { src: avatar3, status: "secondery" },
+      name: "aldana aldana",
+
+      phone: "0944444444",
+      account: 1000234578400,
+      status: "orderd",
+    },
+    {
+      avatar: { src: avatar4, status: "success" },
+
+      name: "eyob eyob",
+
+      phone: "095566677888",
+      account: 1000234578400,
+      status: "deliverd",
+    },
+    {
+      avatar: { src: avatar6, status: "danger" },
+
+      name: "bright bright",
+
+      phone: "0919863245",
+      account: 1000234578400,
+      status: "cancelled",
+    },
+    {
+      avatar: { src: avatar1, status: "success" },
+      name: "lidya desalegn",
+      phone: "0911111111",
+      account: 1000234578400,
+      status: "deliverd",
+    },
+    {
+      avatar: { src: avatar5, status: "danger" },
+
+      name: "seble stayew",
+
+      phone: "0922222222",
+      account: 1000234578400,
+      status: "pending",
+    },
+  ];
+
+  return (
+    <>
+      <CRow>
+        <CCol xs>
+          <CCard className="mb-4">
+            <CCol md={12}>
+              <h5 style={{ marginTop: "2%", marginLeft: "2%" }}>Drivers</h5>
+            </CCol>
+            <CCardBody>
+              <br />
+
+              <CTable align="middle" className="mb-0 border" hover responsive>
+                <CTableHead color="light">
+                  <CTableRow>
+                    <CTableHeaderCell className="text-center">
+                      <CIcon icon={cilPeople} />
+                    </CTableHeaderCell>
+                    <CTableHeaderCell>Name</CTableHeaderCell>
+                    <CTableHeaderCell className="text-center">
+                      Phone No
+                    </CTableHeaderCell>
+                    <CTableHeaderCell>Account</CTableHeaderCell>
+                    <CTableHeaderCell className="text-center">
+                      Status
+                    </CTableHeaderCell>
+                    <CTableHeaderCell>...</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  {tableExample.map((item, index) => (
+                    <CTableRow v-for="item in tableItems" key={index}>
+                      <CTableDataCell className="text-center">
+                        <CAvatar
+                          size="md"
+                          src={item.avatar.src}
+                          status={item.avatar.status}
+                        />
+                      </CTableDataCell>
+
+                      <CTableDataCell>
+                        <div>{item.name}</div>
+                      </CTableDataCell>
+
+                      <CTableDataCell className="text-center">
+                        {item.phone}
+                      </CTableDataCell>
+
+                      <CTableDataCell>
+                        <div>{item.account}</div>
+                      </CTableDataCell>
+
+                      <CTableDataCell className="text-center">
+                        {item.status}
+                      </CTableDataCell>
+
+                      <CTableDataCell>
+                        <CHeaderNav className="ms-3">
+                          <TableDropdown />
+                        </CHeaderNav>
+                      </CTableDataCell>
+                    </CTableRow>
+                  ))}
+                </CTableBody>
+              </CTable>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  );
+};
+
+export default Drivers;
