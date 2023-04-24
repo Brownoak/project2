88161aa3004cb8e84cfbae5cc1c7a619cc544590
Brownoak <helanii93@gmail.com>
2023-04-24 09:41:52 +0300

    updated navigation file

--- src/views/_nav.js
+++ src/views/_nav.js
@@ -1,48 +1,92 @@
-import React from 'react'
-import CIcon from '@coreui/icons-react'
+import React from "react";
+import CIcon from "@coreui/icons-react";
 import {
   cilSpeedometer,
   cilCart,
   cilReload,
-  cilNotes
-} from '@coreui/icons'
+  cilNotes,
+  cilBike,
+  cilGroup,
+  cilUser,
+  cilCheckCircle,
+  cilBasket,
+} from "@coreui/icons";
 
-import { CNavItem, CNavTitle } from '@coreui/react'
+import { CNavGroup, CNavItem, CNavTitle } from "@coreui/react";
 
 const _nav = [
   {
     component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
+    name: "Dashboard",
+    to: "/dashboard",
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW',
-    // },
   },
+
   {
     component: CNavTitle,
-    name: 'Order Information',
+    name: "User Information",
   },
-  {
-    component: CNavItem,
-    name: 'Orders',
-    to: '/order',
-    icon: <CIcon icon={cilCart} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Progress',
-    to: '/progress',
-    icon: <CIcon icon={cilReload} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Done',
-    to: '/done',
-    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
-  },
-  
-]
 
-export default _nav
+  {
+    component: CNavGroup,
+    name: "User Managment",
+    to: "/customer",
+    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: "Drivers",
+        icon: <CIcon icon={cilBike} customClassName="nav-icon"/>,
+        to: "/drivers",
+      },
+      {
+        component: CNavItem,
+        name: "Service Providers",
+        icon: <CIcon icon={cilNotes} customClassName="nav-icon"/>,
+        to: "/providers",
+      },
+      {
+        component: CNavItem,
+        name: "Employees",
+        icon: <CIcon icon={cilGroup} customClassName="nav-icon"/>,
+        to: "/employees",
+      },
+    ],
+  },
+
+  {
+    component: CNavTitle,
+    name: "Order Information",
+  },
+
+  {
+    component: CNavGroup,
+    name: "Order Managment",
+    to: "/order",
+    icon: <CIcon icon={cilBasket} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: "Orders",
+        icon: <CIcon icon={cilCart} customClassName="nav-icon"/>,
+        to: "/order",
+      },
+      {
+        component: CNavItem,
+        name: "Progress",
+        icon: <CIcon icon={cilReload} customClassName="nav-icon"/>,
+        to: "/progress",
+      },
+      {
+        component: CNavItem,
+        name: "Done",
+        icon: <CIcon icon={cilCheckCircle} customClassName="nav-icon"/>,
+        to: "/done",
+      },
+    ],
+  },
+
+  
+];
+
+export default _nav;
